164a51327a047bb70ada0b95420b66d7c4257947
Allen Huang <allenhuang@Allens-Laptop.lan>
2024-01-28 11:57:29 -0800

    Add Card and Deck structs to construct a deck

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,44 @@
 use text_io::read;
 
-fn main() {
-    println!("How much do you want to bet?");
+struct Card {
+    suit: char,
+    rank: String,
+    value: u8,
+    count_value: u8,
+}
 
-    let bet: i32 = read!();
-    println!("You bet ${}", bet)
+struct Deck {
+    cards: Vec<Card>,
+}
+
+fn main() {
+    let suits = ['C', 'H', 'S', 'D'];
+    let ranks = [
+        "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A",
+    ];
+
+    let mut deck: Deck = Deck {
+        cards: Vec::<Card>::new(),
+    };
+
+    for suit in suits {
+        for rank in ranks {
+            let card = Card {
+                suit,
+                rank: String::from(rank),
+                value: 0,
+                count_value: 0,
+            };
+            deck.cards.push(card)
+        }
+    }
+
+    println!("Deck size: {}", deck.cards.len());
+
+    loop {
+        println!("How much do you want to bet?");
+
+        let bet: i32 = read!();
+        println!("You bet ${}", bet)
+    }
 }
